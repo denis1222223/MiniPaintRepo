915aa737221285a5887a81291bc7d707052c2c4c
denis1222223 <denis1222223@mail.ru>
2016-09-06 19:51:04 +0300

    color picker

--- MiniPaintProject/MiniPaintProject.cpp
+++ MiniPaintProject/MiniPaintProject.cpp
@@ -1,21 +1,192 @@
-// MiniPaintProject.cpp: определяет точку входа для приложения.
-//
-
 #include "stdafx.h"
-#include "MiniPaintProject.h"
+#include <windows.h>
+#include <CommDlg.h>
+#include <stdio.h>
+#include <string>
+#include "resource.h"
 
-#define MAX_LOADSTRING 100
+using namespace std;
 
-// Глобальные переменные:
 HINSTANCE hInst;                                // текущий экземпляр
 WCHAR szTitle[MAX_LOADSTRING];                  // Текст строки заголовка
 WCHAR szWindowClass[MAX_LOADSTRING];            // имя класса главного окна
 
-// Отправить объявления функций, включенных в этот модуль кода:
-ATOM                MyRegisterClass(HINSTANCE hInstance);
-BOOL                InitInstance(HINSTANCE, int);
-LRESULT CALLBACK    WndProc(HWND, UINT, WPARAM, LPARAM);
-INT_PTR CALLBACK    About(HWND, UINT, WPARAM, LPARAM);
+// menu / color dialog
+static HPEN hPen;
+static HBRUSH hBrush;
+static COLORREF  crCustColor[16];
+static HDC hCompatibleDC = 0;
+static HDC hBitmapDC = 0;
+static HDC hdc1;
+static int width = 0;
+
+
+
+CHOOSECOLOR GetColorDialog(HWND hWnd) {
+	CHOOSECOLOR chooseColor = { 0 };
+	chooseColor.lStructSize = sizeof(CHOOSECOLOR);
+	chooseColor.hInstance = NULL;
+	chooseColor.hwndOwner = hWnd;
+	chooseColor.lpCustColors = crCustColor;
+	chooseColor.Flags = CC_RGBINIT | CC_FULLOPEN;
+	chooseColor.lCustData = 0L;
+	chooseColor.lpfnHook = NULL;
+	chooseColor.rgbResult = RGB(0x80, 0x80, 0x80);
+	chooseColor.lpTemplateName = NULL;
+	return chooseColor;
+}
+
+void OnChangePenColor(HWND hWnd) {
+	CHOOSECOLOR chooseColor = GetColorDialog(hWnd);
+	if (ChooseColor(&chooseColor))
+	{
+		DeleteObject(hPen);
+		hPen = CreatePen(PS_SOLID, width, chooseColor.rgbResult);
+		DeleteObject(SelectObject(hCompatibleDC, hPen));
+		DeleteObject(SelectObject(hBitmapDC, hPen));
+		DeleteObject(SelectObject(hdc1, hPen));
+	}
+}
+
+void OnChangeFillColor(HWND hWnd) {
+	CHOOSECOLOR chooseColor = GetColorDialog(hWnd);
+	if (ChooseColor(&chooseColor))
+	{
+		DeleteObject(hBrush);
+		hBrush = CreateSolidBrush(chooseColor.rgbResult);
+		DeleteObject(SelectObject(hCompatibleDC, hBrush));
+		DeleteObject(SelectObject(hBitmapDC, hBrush));
+		DeleteObject(SelectObject(hdc1, hBrush));
+	}
+}
+
+void Line() {
+	id = ID_BUTTONLINE;
+	scale = 1;
+	xBegin = 0;
+	yBegin = 0;
+	bPoly = false;
+	bPolyline = false;
+	InvalidateRect(hWnd, NULL, FALSE);
+	break;
+}
+
+// Aboutbox click handler.
+INT_PTR CALLBACK About(HWND hDlg, UINT message, WPARAM wParam, LPARAM lParam)
+{
+	UNREFERENCED_PARAMETER(lParam);
+	switch (message)
+	{
+	case WM_INITDIALOG:
+		return (INT_PTR)TRUE;
+	case WM_COMMAND:
+		if (LOWORD(wParam) == IDOK || LOWORD(wParam) == IDCANCEL)
+		{
+			EndDialog(hDlg, LOWORD(wParam));
+			return (INT_PTR)TRUE;
+		}
+		break;
+	}
+	return (INT_PTR)FALSE;
+}
+
+void OnMenuClick(HWND hWnd, WORD itemId) {
+	switch (itemId) {
+	case IDM_ABOUT:
+		DialogBox(hInst, MAKEINTRESOURCE(IDD_ABOUTBOX), hWnd, About);
+		break;
+	case ID_FILE_OPEN:
+		MessageBox(NULL, _T("open"), _T(""), NULL);
+		break;
+	case ID_FILE_SAVE:
+		MessageBox(NULL, _T("save"), _T(""), NULL);
+		break;
+	case IDM_EXIT:
+		DestroyWindow(hWnd);
+		break;
+	case ID_FILE_PRINT:
+		MessageBox(NULL, _T("print"), _T(""), NULL);
+		break;
+	case ID_EDIT_UNDO:
+		MessageBox(NULL, _T("undo"), _T(""), NULL);
+		break; 
+	case ID_COLOR_PEN:
+		OnChangePenColor(hWnd);
+		break;
+	case ID_COLOR_FILL:
+		OnChangeFillColor(hWnd);
+		break;
+	case ID_INSTRUMENT_LINE:
+		Line();
+		break;
+	}
+}
+
+//   saving instance processing and creating main window
+BOOL InitInstance(HINSTANCE hInstance, int nCmdShow)
+{
+	hInst = hInstance; // saving instance discriptor to the global variable
+
+	HWND hWnd = CreateWindowW(szWindowClass, szTitle, WS_OVERLAPPEDWINDOW,
+		CW_USEDEFAULT, 0, CW_USEDEFAULT, 0, nullptr, nullptr, hInstance, nullptr);
+
+	if (!hWnd)
+	{
+		return FALSE;
+	}
+
+	ShowWindow(hWnd, nCmdShow);
+	UpdateWindow(hWnd);
+
+	return TRUE;
+}
+
+//  process messages in main window
+LRESULT CALLBACK WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)
+{
+	switch (message)
+	{
+	case WM_COMMAND:
+	 	OnMenuClick(hWnd, LOWORD(wParam));
+		break;
+	case WM_PAINT:
+	{
+		PAINTSTRUCT ps;
+		HDC hdc = BeginPaint(hWnd, &ps);
+		// TODO: Добавьте сюда любой код прорисовки, использующий HDC...
+		EndPaint(hWnd, &ps);
+	}
+	break;
+	case WM_DESTROY:
+		PostQuitMessage(0);
+		break;
+	default:
+		return DefWindowProc(hWnd, message, wParam, lParam);
+	}
+	return 0;
+}
+
+//  Registration window's class.
+ATOM MyRegisterClass(HINSTANCE hInstance)
+{
+	WNDCLASSEXW wcex;
+
+	wcex.cbSize = sizeof(WNDCLASSEX);
+
+	wcex.style = CS_HREDRAW | CS_VREDRAW;
+	wcex.lpfnWndProc = WndProc;
+	wcex.cbClsExtra = 0;
+	wcex.cbWndExtra = 0;
+	wcex.hInstance = hInstance;
+	wcex.hIcon = LoadIcon(hInstance, MAKEINTRESOURCE(IDI_MINIPAINTPROJECT));
+	wcex.hCursor = LoadCursor(nullptr, IDC_ARROW);
+	wcex.hbrBackground = (HBRUSH)(COLOR_WINDOW + 1);
+	wcex.lpszMenuName = MAKEINTRESOURCEW(IDI_MINIPAINTPROJECT);
+	wcex.lpszClassName = szWindowClass;
+	wcex.hIconSm = LoadIcon(wcex.hInstance, MAKEINTRESOURCE(IDI_MINIPAINTPROJECT));
+
+	return RegisterClassExW(&wcex);
+}
 
 int APIENTRY wWinMain(_In_ HINSTANCE hInstance,
                      _In_opt_ HINSTANCE hPrevInstance,
@@ -53,128 +224,4 @@ int APIENTRY wWinMain(_In_ HINSTANCE hInstance,
     }
 
     return (int) msg.wParam;
-}
-
-
-
-//
-//  ФУНКЦИЯ: MyRegisterClass()
-//
-//  НАЗНАЧЕНИЕ: регистрирует класс окна.
-//
-ATOM MyRegisterClass(HINSTANCE hInstance)
-{
-    WNDCLASSEXW wcex;
-
-    wcex.cbSize = sizeof(WNDCLASSEX);
-
-    wcex.style          = CS_HREDRAW | CS_VREDRAW;
-    wcex.lpfnWndProc    = WndProc;
-    wcex.cbClsExtra     = 0;
-    wcex.cbWndExtra     = 0;
-    wcex.hInstance      = hInstance;
-    wcex.hIcon          = LoadIcon(hInstance, MAKEINTRESOURCE(IDI_MINIPAINTPROJECT));
-    wcex.hCursor        = LoadCursor(nullptr, IDC_ARROW);
-    wcex.hbrBackground  = (HBRUSH)(COLOR_WINDOW+1);
-    wcex.lpszMenuName   = MAKEINTRESOURCEW(IDI_MINIPAINTPROJECT);
-    wcex.lpszClassName  = szWindowClass;
-    wcex.hIconSm        = LoadIcon(wcex.hInstance, MAKEINTRESOURCE(IDI_SMALL));
-
-    return RegisterClassExW(&wcex);
-}
-
-//
-//   ФУНКЦИЯ: InitInstance(HINSTANCE, int)
-//
-//   НАЗНАЧЕНИЕ: сохраняет обработку экземпляра и создает главное окно.
-//
-//   КОММЕНТАРИИ:
-//
-//        В данной функции дескриптор экземпляра сохраняется в глобальной переменной, а также
-//        создается и выводится на экран главное окно программы.
-//
-BOOL InitInstance(HINSTANCE hInstance, int nCmdShow)
-{
-   hInst = hInstance; // Сохранить дескриптор экземпляра в глобальной переменной
-
-   HWND hWnd = CreateWindowW(szWindowClass, szTitle, WS_OVERLAPPEDWINDOW,
-      CW_USEDEFAULT, 0, CW_USEDEFAULT, 0, nullptr, nullptr, hInstance, nullptr);
-
-   if (!hWnd)
-   {
-      return FALSE;
-   }
-
-   ShowWindow(hWnd, nCmdShow);
-   UpdateWindow(hWnd);
-
-   return TRUE;
-}
-
-//
-//  ФУНКЦИЯ: WndProc(HWND, UINT, WPARAM, LPARAM)
-//
-//  НАЗНАЧЕНИЕ:  обрабатывает сообщения в главном окне.
-//
-//  WM_COMMAND — обработать меню приложения
-//  WM_PAINT — отрисовать главное окно
-//  WM_DESTROY — отправить сообщение о выходе и вернуться
-//
-//
-LRESULT CALLBACK WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)
-{
-    switch (message)
-    {
-    case WM_COMMAND:
-        {
-            int wmId = LOWORD(wParam);
-            // Разобрать выбор в меню:
-            switch (wmId)
-            {
-            case IDM_ABOUT:
-                DialogBox(hInst, MAKEINTRESOURCE(IDD_ABOUTBOX), hWnd, About);
-                break;
-            case IDM_EXIT:
-                DestroyWindow(hWnd);
-                break;
-            default:
-                return DefWindowProc(hWnd, message, wParam, lParam);
-            }
-        }
-        break;
-    case WM_PAINT:
-        {
-            PAINTSTRUCT ps;
-            HDC hdc = BeginPaint(hWnd, &ps);
-            // TODO: Добавьте сюда любой код прорисовки, использующий HDC...
-            EndPaint(hWnd, &ps);
-        }
-        break;
-    case WM_DESTROY:
-        PostQuitMessage(0);
-        break;
-    default:
-        return DefWindowProc(hWnd, message, wParam, lParam);
-    }
-    return 0;
-}
-
-// Обработчик сообщений для окна "О программе".
-INT_PTR CALLBACK About(HWND hDlg, UINT message, WPARAM wParam, LPARAM lParam)
-{
-    UNREFERENCED_PARAMETER(lParam);
-    switch (message)
-    {
-    case WM_INITDIALOG:
-        return (INT_PTR)TRUE;
-
-    case WM_COMMAND:
-        if (LOWORD(wParam) == IDOK || LOWORD(wParam) == IDCANCEL)
-        {
-            EndDialog(hDlg, LOWORD(wParam));
-            return (INT_PTR)TRUE;
-        }
-        break;
-    }
-    return (INT_PTR)FALSE;
 }
